201870572a529a8c53220dd9188aaeacc71b7c79
ookkamkoo <ookkamkoo@hotmail.com>
2024-11-08 17:26:22 +0700

    update migration

--- app/route/route.go
+++ app/route/route.go
@@ -34,7 +34,7 @@ func SetRoute(app *fiber.App) {
 
 	api.Get("/game-list/:categoryId/:productId", middlewares.GameSeviceMiddleware(), controllers.GameList)
 	// api.Post("/launch-game", controllers.LaunchGame)
-	api.Post("launch-games/:productId", middlewares.GameSeviceMiddleware(), controllers.LaunchGames)
+	api.Post("/launch-games/:productId", middlewares.GameSeviceMiddleware(), controllers.LaunchGames)
 	api.Post("/settingPg", controllers.SettingGamePg100)
 	api.Get("/getRefoundLost", middlewares.GameSeviceMiddleware(), controllers.GetBetWinLossSummary)
 	api.Post("/getReportGame", middlewares.GameSeviceMiddleware(), controllers.GetReportGame)
